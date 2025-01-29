6e9c66954b5ee3ec389b117ea1b94a4fd628852f
Ведерников Сергей <swnike@mail.ru>
2025-01-29 20:38:57 +0500

    Добавьте файлы проекта.

---
+++ Task_13_6_2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_13_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\swnike\\Downloads\\cdev_Text.txt");
+
+            char[] delimiters = new char[] { ' ', ',', '.', '-', '\r', '\n' };
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            // выводим количество
+            Console.WriteLine($"Кол-во слов в массиве {words.Length}");
+
+            var booklist = new List<string>();
+            var linkedbooklist = new LinkedList<string>();
+
+            foreach (var word in words)
+            {
+                booklist.Add(word);
+                linkedbooklist.AddLast(word);
+            }
+            Console.WriteLine($"Кол-во слов в списке {booklist.Count}");
+
+
+
+        }
+    }
+}
