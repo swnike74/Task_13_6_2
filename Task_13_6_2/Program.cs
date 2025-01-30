2caa3d31cada80aab76f96cf4efac794112f4da9
Ведерников Сергей <swnike@mail.ru>
2025-01-30 22:54:18 +0500

    update4

--- Task_13_6_2/Program.cs
+++ Task_13_6_2/Program.cs
@@ -6,7 +6,7 @@ namespace Task_13_6_2
     {
         static void Main(string[] args)
         {
-            string text = File.ReadAllText("C:\\Users\\swnik\\Downloads\\cdev_Text.txt");
+            string text = File.ReadAllText("C:\\Users\\swnike\\Downloads\\cdev_Text.txt");
 
             var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
 
@@ -16,22 +16,12 @@ namespace Task_13_6_2
             Console.WriteLine($"Кол-во слов в массиве {words.Length}");
 
             var booklist = new List<string>();
-            var linkedbooklist = new LinkedList<string>();
 
             foreach (var word in words)
             {
                 booklist.Add(word);
-                linkedbooklist.AddLast(word);
             }
             Console.WriteLine($"Кол-во слов в списке {booklist.Count}");
-
-            // Создаем хэш-сет, передавая в конструктор наш массив
-            HashSet<string> hSet = new HashSet<string>(words);
-            Console.WriteLine("Длина хэш-сета " + hSet.Count);
-
-            var digit = new[] {"1","2", "3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26" };
-            hSet.ExceptWith(digit);
-            Console.WriteLine("Длина хэш-сета без цифр " + hSet.Count);
 
             var dict = new Dictionary<string, int>();
             foreach (var item in booklist)
@@ -42,22 +32,17 @@ namespace Task_13_6_2
                     dict.Add(item, 1);
             }
             
-            var t = dict.Values;
-            var s = dict.Keys;
-            
-            var dict2 = new Dictionary<int, string>();
             int j = 0;
-            string st;
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
                 int max = -100;
                 string keymax = "";
                 foreach (var item in dict.Keys)
                 {
-                    var dig = dict[item];
-                    if (dig > max)
+                    var quantwords = dict[item];
+                    if (quantwords > max)
                     {
-                        max = dig;
+                        max = quantwords;
                         keymax = item;
                     }
                 }
@@ -65,10 +50,7 @@ namespace Task_13_6_2
                 j++;
                 dict.Remove(keymax);
             }
-            var sdict = new SortedDictionary<string, int>(dict);
-
             Console.WriteLine();
-
         }
     }
 }
